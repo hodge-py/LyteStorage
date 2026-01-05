7d7892adf646aecbbe2cb03a35e8fa1f54acb2cd
hodge-py <105604814+hodge-py@users.noreply.github.com>
2026-01-05 16:16:31 -0700

    c

--- PHP/view/dashboard.php
+++ PHP/view/dashboard.php
@@ -18,12 +18,25 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     <link rel="stylesheet" href="../static/css/dashboard.css">
 </head>
 <body>
-    <div class="container">
-        
-
-
+    
+    <div class="header">
+        <h1 id="title">LyteStorage</h1>
+        <a id="logoutButton" onclick="handleLogout()" class="logout-button">Logout</a>
     </div>
 
+    <div class="container">
+        <div id="fileSystem">
+            <div id="file header">
+                
+            </div>
+
+
+
+        </div>
+       
+
+    
+    </div>
 
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js" integrity="sha512-XMVd28F1oH/O71fzwBnV7HucLxVwtxf26XV8P4wPk26EDxuGZ91N8bsOttmnomcCD3CS5ZMRL50H0GgOHvegtg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
